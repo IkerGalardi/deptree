bc39582d7149c886be2cd84fdec3559f795f6536
Iker Galardi <contacto.ikergalardi@gmail.com>
2023-08-28 23:04:38 +0200

    Pass printing format to iteration function

--- deptree.py
+++ deptree.py
@@ -35,22 +35,22 @@ def get_binary_dependencies(binary):
     
     return parse_ldd_output(proc.stdout.decode('utf-8'))
 
-def print_dependencies_recursivelly(binary, full_path):
+def print_dependencies_recursivelly(binary, full_path, format):
     deps = get_binary_dependencies(binary)
 
     visited_dependencies.append(binary)
 
     for dependency in deps:
         if full_path == True:
-            print(f'"{binary}" -> "{dependency}"')
+            print(format.format(binary = binary, dependency = dependency))
         else:
             bin_basename = os.path.basename(binary)
             dep_basename = os.path.basename(dependency)
-            print(f'"{bin_basename}" -> "{dep_basename}"')
+            print(format.format(binary = bin_basename, dependency = dep_basename))
 
         if dependency in visited_dependencies:
             return
-        print_dependencies_recursivelly(dependency, full_path)
+        print_dependencies_recursivelly(dependency, full_path, format)
 
 def main():
     parser = argparse.ArgumentParser(prog='deptree',
@@ -66,8 +66,8 @@ def main():
         print(f'deptree: format \'{arguments.format}\' is not supported, try {SUPPORTED_PRINT_FORMATS} formats instead')
 
     if arguments.format == 'dot':
-        print_dependencies_recursivelly(arguments.elf, arguments.print_full_path)
-    elif arguments.format == 'list-newline':
+        print_dependencies_recursivelly(arguments.elf, arguments.print_full_path, '"{binary}" -> "{dependency}"')
+    elif arguments.format == 'list':
         print('deptree: not implemented yet')
 
 if __name__ == '__main__':
